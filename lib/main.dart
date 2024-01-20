cb226f1fd9bf3ed208f71cae091ac71ca547c30c
NR5P <orangev8zcar@gmail.com>
2024-01-20 08:19:04 -0600

    the new calc for grid square seems to be working

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,41 +11,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GPS and Time App',
-      home: GpsTimeHomePage(),
+      title: 'Ham Radio Grid Square App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Ham Radio Grid Square Finder'),
     );
   }
 }
 
-class GpsTimeHomePage extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, this.title}) : super(key: key);
+  final String? title;
+
   @override
-  _GpsTimeHomePageState createState() => _GpsTimeHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _GpsTimeHomePageState extends State<GpsTimeHomePage> {
-  String _timeString = "";
-  String _locationString = "Getting location...";
+class _MyHomePageState extends State<MyHomePage> {
+  String gridSquare = "";
+  String currentTime = "";
 
   @override
   void initState() {
     super.initState();
-    _getTime();
-    _getLocation();
+    getCurrentLocation();
+    getCurrentTime();
   }
 
-  void _getTime() {
-    final String formattedDateTime =
-        DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now().toUtc());
-    setState(() {
-      _timeString = "GMT Time: $formattedDateTime";
-    });
-  }
-
-  void _getLocation() async {
+  void getCurrentLocation() async {
     bool serviceEnabled;
     LocationPermission permission;
 
-    // Check if location services are enabled
+    // Check if location services are enabled.
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
       return Future.error('Location services are disabled.');
@@ -61,33 +60,76 @@ class _GpsTimeHomePageState extends State<GpsTimeHomePage> {
     if (permission == LocationPermission.deniedForever) {
       return Future.error(
           'Location permissions are permanently denied, we cannot request permissions.');
-    } 
+    }
 
-    // When permissions are granted
-    Position position = await Geolocator.getCurrentPosition();
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
     setState(() {
-      _locationString = "Latitude: ${position.latitude}, Longitude: ${position.longitude}";
+      gridSquare = calculateGridSquare(position.latitude, position.longitude);
     });
+  }
+
+  void getCurrentTime() {
+    Timer.periodic(Duration(seconds: 1), (Timer t) => updateTime());
+  }
+
+  void updateTime() {
+    setState(() {
+      currentTime = DateFormat('HH:mm:ss').format(DateTime.now().toUtc());
+    });
+  }
+
+  /*
+  String calculateGridSquare(double latitude, double longitude) {
+    int lat = (latitude + 90).toInt() * 60;
+    int lon = (longitude + 180).toInt() * 60;
+    String gridSquare = String.fromCharCode(65 + (lon / 120).floor()) +
+        String.fromCharCode(65 + (lat / 120).floor()) +
+        String.fromCharCode(48 + ((lon % 120) / 10).floor()) +
+        String.fromCharCode(48 + ((lat % 120) / 10).floor());
+    return gridSquare;
+  }
+  */
+
+  String calculateGridSquare(double latitude, double longitude) {
+    // Longitude calculations
+    int longIndex = ((longitude + 180) / 20).floor();
+    String longLetter = String.fromCharCode('A'.codeUnitAt(0) + longIndex);
+    int longNumber = (((longitude + 180) % 20) / 2).floor();
+    int longSubLetterIndex = ((((longitude + 180) % 20) % 2) * 12).floor();
+    String longSubLetter = String.fromCharCode('a'.codeUnitAt(0) + longSubLetterIndex);
+
+    // Latitude calculations
+    int latIndex = ((latitude + 90) / 10).floor();
+    String latLetter = String.fromCharCode('A'.codeUnitAt(0) + latIndex);
+    int latNumber = ((latitude + 90) % 10).floor();
+    int latSubLetterIndex = ((((latitude + 90) % 10) % 1) * 24).floor();
+    String latSubLetter = String.fromCharCode('a'.codeUnitAt(0) + latSubLetterIndex);
+
+    // Combining the results
+    return longLetter + latLetter + longNumber.toString() + latNumber.toString() + longSubLetter + latSubLetter;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('GPS and Time App'),
+        title: Text(widget.title!),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(_timeString, style: TextStyle(fontSize: 20)),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(_locationString, style: TextStyle(fontSize: 20)),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Current Time (GMT): $currentTime',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Grid Square: $gridSquare',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
       ),
     );
   }
